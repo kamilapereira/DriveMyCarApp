617f36f7ef314ff88c241b885e579a2f6af15072
kamilapereira <pereira.kamila@outlook.com>
2024-02-07 15:57:50 +0000

    Add react-native-dotenv package and update dependencies

---
+++ config/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import Constants from "expo-constants";
+//import "dotenv/config";
+
+const firebaseConfig = {
+
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+}
+
+initializeApp(firebaseConfig);
+export const auth = getAuth();
+export const database = getFirestore();
